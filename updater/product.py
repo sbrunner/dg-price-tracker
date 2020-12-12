87e85da5beeec148f3b51655e60c1f4fa5dcefe5
Stéphane Brunner <stephane.brunner@camptocamp.com>
2020-12-12 18:14:45 +0100

    Add checks

--- updater/product.py
+++ updater/product.py
@@ -1,24 +1,23 @@
 from requests_html import HTMLSession
 
-
 BASE_URLS = {
-    'g': 'https://www.galaxus.ch/en/s2/product/',
-    'd': 'https://www.digitec.ch/en/s2/product/',
+    "g": "https://www.galaxus.ch/en/s2/product/",
+    "d": "https://www.digitec.ch/en/s2/product/",
 }
 
 session = HTMLSession()
 
 
 def get_info(product_id):
-    site, product = product_id.split('.')
+    site, product = product_id.split(".")
     url = BASE_URLS.get(site) + str(product)
     r = session.get(url)
     if r.ok:
-        brand = r.html.find("meta[property='og:brand']", first=True).attrs.get('content')
-        title = r.html.find("meta[property='og:title']", first=True).attrs.get('content')
-        image = r.html.find("meta[property='og:image']", first=True).attrs.get('content')
+        brand = r.html.find("meta[property='og:brand']", first=True).attrs.get("content")
+        title = r.html.find("meta[property='og:title']", first=True).attrs.get("content")
+        image = r.html.find("meta[property='og:image']", first=True).attrs.get("content")
         # if the product is currently unavailable, the price is not set
         price_node = r.html.find("meta[property='product:price:amount']", first=True)
-        price = price_node.attrs.get('content') if price_node is not None else 0
+        price = price_node.attrs.get("content") if price_node is not None else 0
 
         return brand, title, image, float(price)
--- updater/updater.py
+++ updater/updater.py
@@ -6,65 +6,63 @@ import os
 import re
 
 import requests
-
 from product import get_info
 
+CONTENTS_API_URL = "https://api.github.com/repos/sbrunner/dg-price-tracker/contents/%s?ref=gh-pages"
 
-CONTENTS_API_URL = 'https://api.github.com/repos/sbrunner/dg-price-tracker/contents/%s?ref=gh-pages'
-
-GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 
 session = requests.Session()
 if GITHUB_TOKEN is not None:
-    session.headers.update({
-        'Authorization': 'token %s' % GITHUB_TOKEN
-    })
+    session.headers.update({"Authorization": "token %s" % GITHUB_TOKEN})
+
 
 def push_commit(path, content, message, sha):
-   if GITHUB_TOKEN is not None:
+    if GITHUB_TOKEN is not None:
         commit = {
-            'message': message,
-            'content': base64.b64encode(bytes(content, 'utf-8')).decode('ascii'),
-            'sha': sha,
-            'branch': 'gh-pages',
+            "message": message,
+            "content": base64.b64encode(bytes(content, "utf-8")).decode("ascii"),
+            "sha": sha,
+            "branch": "gh-pages",
         }
         r = session.put(CONTENTS_API_URL % path, data=json.dumps(commit))
         assert r.ok, r.text
 
+
 def update_price(product_id, price):
-    today = datetime.datetime.now().strftime('%Y-%m-%d')
-    path = 'prices/%s.csv' % product_id
+    today = datetime.datetime.now().strftime("%Y-%m-%d")
+    path = "prices/%s.csv" % product_id
     sha = None
     entry = session.get(CONTENTS_API_URL % path)
     if entry.status_code == 404:
-        logging.info('create price file for %s' % product_id)
-        prices = ['date,price']
+        logging.info("create price file for %s" % product_id)
+        prices = ["date,price"]
     else:
         meta = entry.json()
-        download_url = meta.get('download_url')
-        sha = meta.get('sha')
-        prices = session.get(download_url).text.split('\n')
+        download_url = meta.get("download_url")
+        sha = meta.get("sha")
+        prices = session.get(download_url).text.split("\n")
         # remove empty entries
         prices = list(filter(None, prices))
     last = prices[-1]
     # avoid duplicated entries for the same day
     if not (last.startswith(today) and last.endswith(str(price))):
-        prices.append('%s,%s' % (today, price))
-        push_commit(path, '\n'.join(prices), '[skip ci] Update product price', sha)
+        prices.append("%s,%s" % (today, price))
+        push_commit(path, "\n".join(prices), "[skip ci] Update product price", sha)
     else:
-        logging.info('no change for %s' % product_id)
+        logging.info("no change for %s" % product_id)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     # get all tracked products
-    entry = session.get(CONTENTS_API_URL % 'products.csv').json()
-    content = session.get(entry.get('download_url')).text.split('\n')
+    entry = session.get(CONTENTS_API_URL % "products.csv").json()
+    content = session.get(entry.get("download_url")).text.split("\n")
     content = list(filter(None, content))
     for i, product in enumerate(content[1:], 1):
         # too lazy to use csv module
-        product_id = re.match(r'^([^,]*)', product).group(1)
-        assert product_id, 'product_id is undefined'
+        product_id = re.match(r"^([^,]*)", product).group(1)
+        assert product_id, "product_id is undefined"
         brand, title, image, price = get_info(product_id)
         logging.info('got product info: %s "%s %s": %s CHF' % (product_id, brand, title, price))
         update_price(product_id, price)
@@ -72,4 +70,4 @@ if __name__ == '__main__':
         # 'title' may contains commas
         content[i] = '%s,"%s %s",%s,%s' % (product_id, brand, title.replace('"', '""'), price, image)
 
-    push_commit(entry.get('path'), '\n'.join(content), '[skip ci] Update products.csv', entry.get('sha'))
+    push_commit(entry.get("path"), "\n".join(content), "[skip ci] Update products.csv", entry.get("sha"))
